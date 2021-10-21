1ad5a2bcef65e227f2aa4b738ae66bb53de3f5f7
badass1901 <59369746+badass1901@users.noreply.github.com>
2021-10-21 07:05:44 -0700

    Add files via upload

---
+++ src/components/Auth.js
@@ -0,0 +1,5 @@
+const socialMediaAuth = () => {
+
+};
+
+export default socialMediaAuth;
---
+++ src/components/AuthMethods.js
@@ -0,0 +1,4 @@
+// import firebase from "./firebase"
+// import 'firebase/compat/auth'
+
+// const facebookProvider = new 
