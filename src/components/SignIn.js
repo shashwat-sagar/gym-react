a8f636ba13687e454faa3323b892c89e669facc2
badass1901 <59369746+badass1901@users.noreply.github.com>
2021-10-21 19:39:23 +0530

    Update SignIn.js

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,52 +1,61 @@
 import React, { useEffect, useState } from "react";
-import { Link, useHistory } from "react-router-dom";
-import { auth, signInWithEmailAndPassword, signInWithGoogle } from "./firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { Link, useHistory } from "react-router-dom";
+import {
+  auth,
+  facebookProvider,
+  registerWithEmailAndPassword,
+  signInWithGoogle,
+  signInWithFacebook
+} from "./firebase";
 
 function SignIn() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [user, loading] = useAuthState(auth);
-  const history = useHistory();
-  useEffect(() => {
-    if (loading) {
-      // maybe trigger a loading screen
-      return;
-    }
-    if (user) history.replace("/dashboard");
-  });
-
+  const handleOnClick = async (provider) => {
+    const res = await signInWithFacebook(provider);
+    console.log(res)
+  }
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [name, setName] = useState("");
+    const [user, loading, error] = useAuthState(auth);
+    const history = useHistory();
+    const register = () => {
+      if (!name) alert("Please enter name");
+      registerWithEmailAndPassword(name, email, password);
+    };
+    useEffect(() => {
+      if (loading) return;
+      if (user) history.replace("/dashboard");
+    }, [user, loading]);
   return (
     <div class="container mt-5 mb-5">
       <div class="row d-flex align-items-center justify-content-center">
         <div class="col-md-6">
           <div class="card px-5 py-5">
+            {" "}
+            <span class="circle">
+              <i class="fa fa-check"></i>
+            </span>
             <small>
-              <span id="error" style={{color: 'red'}}></span>
+              <span id="error1" style={{ color: "red" }}></span>
             </small>
             <small>
-              <span id="reset" style={{color: 'greenyellow'}}></span>
+              <span id="success" style={{ color: "green" }}></span>
             </small>
-            <h3 class="mt-3">Login Here!</h3>{" "}
-            <small class="mt-1 text-muted">
-              Welcome to{" "}
-              <font color="#ffc400" size="4px">
-                A
-              </font>
-              thlete{" "}
-              <font color="#ffc400" size="4px">
-                C
-              </font>
-              lub
+            <h3 class="mt-3">Join before it gets too late...</h3>{" "}
+            <small class="mt-2 text-muted">
+              Your Body can stand almost anything.
+              <br />
+              It's your mind that you have to continue.
             </small>
             <div class="form-input">
               {" "}
               <i class="fa fa-envelope"></i>{" "}
               <input
                 type="email"
+                class="form-control"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                class="form-control"
                 placeholder="Email address"
                 id="email"
               />{" "}
@@ -63,16 +72,55 @@ function SignIn() {
                 id="password"
               />{" "}
             </div>
-            <button
-              class="btn btn-primary mt-4 signup "
-              id="buttonSub"
-              onClick={() => signInWithEmailAndPassword(email, password)}
-            >
-              Login
+            <div class="form-input">
+              {" "}
+              <i class="fa fa-user"></i>{" "}
+              <input
+                type="full_name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                class="form-control"
+                placeholder="Full Name"
+                id="full_name"
+              />{" "}
+            </div>
+            <div class="form-input">
+              {" "}
+              <i class="fa fa-user"></i>{" "}
+              <input
+                type="phone"
+                class="form-control"
+                placeholder="Phone"
+                id="phone"
+              />{" "}
+            </div>
+            <div class="form-input">
+              {" "}
+              <i class="fa fa-user"></i>{" "}
+              <input
+                type="age"
+                class="form-control"
+                placeholder="Age"
+                id="age"
+              />{" "}
+            </div>
+            <div class="form-check">
+              {" "}
+              <input
+                class="form-check-input"
+                type="checkbox"
+                value=""
+                id="flexCheckChecked"
+                checked
+              />{" "}
+              <label class="form-check-label" for="flexCheckChecked">
+                {" "}
+                I agree all the statements{" "}
+              </label>{" "}
+            </div>
+            <button class="btn btn-primary mt-4 signup " id="buttonSub" onClick={register}>
+              Register
             </button>
-            <Link to="/reset">
-              Forgot Password
-            </Link>
             <div class="text-center mt-3">
               {" "}
               <span>Or continue with these social profile</span>{" "}
@@ -83,7 +131,7 @@ function SignIn() {
                 <i class="fa fa-google" onClick={signInWithGoogle}></i>
               </span>{" "}
               <span class="social">
-                <i class="fa fa-facebook"></i>
+                <i class="fa fa-facebook" onClick={signInWithFacebook}></i>
               </span>{" "}
               <span class="social">
                 <i class="fa fa-twitter"></i>
@@ -91,10 +139,10 @@ function SignIn() {
             </div>
             <div class="text-center mt-4">
               {" "}
-              <span>New here?</span>{" "}
+              <span>Already a member? </span>{" "}
               <font color="#ffc400">
-                <Link to="/register" class="text-decoration-none">
-                  Join Now
+                <Link to="/premium" class="text-decoration-none">
+                  Login
                 </Link>
               </font>{" "}
             </div>
