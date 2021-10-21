283040e542567e17e0dec94a97e7a53383afb8b9
badass1901 <59369746+badass1901@users.noreply.github.com>
2021-10-21 19:38:03 +0530

    Update firebase.js

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,6 +1,6 @@
-import firebase from "firebase/compat/app"
-import 'firebase/compat/auth'
-import 'firebase/compat/firestore'
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBJorH2XuqKD0z1HZZZpzAl7RFeQARXNYs",
@@ -9,7 +9,7 @@ const firebaseConfig = {
   storageBucket: "athlete-club-e77a1.appspot.com",
   messagingSenderId: "988569236642",
   appId: "1:988569236642:web:c22e378d0266cb608853fb",
-  measurementId: "G-2BJC76PJ16"
+  measurementId: "G-2BJC76PJ16",
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -17,6 +17,8 @@ firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
 const googleProvider = new firebase.auth.GoogleAuthProvider();
+const facebookProvider = new firebase.auth.FacebookAuthProvider();
+
 const signInWithGoogle = async () => {
   try {
     const res = await auth.signInWithPopup(googleProvider);
@@ -38,6 +40,41 @@ const signInWithGoogle = async () => {
     alert(err.message);
   }
 };
+
+//const signInWithFacebook = (provider) => {
+  //return firebase
+    //.auth()
+    //.signInWithPopup(provider)
+    //.then((res) => {
+      //return res.user;
+    //})
+    //.catch((er) => {
+//      return er;
+    //});
+//};
+
+const signInWithFacebook = async () => {
+  try {
+    const res = await auth.signInWithPopup(facebookProvider);
+    const user = res.user;
+    const query = await db
+      .collection("users")
+      .where("uid", "==", user.uid)
+      .get();
+    if (query.docs.length === 0) {
+      await db.collection("users").add({
+        uid: user.uid,
+        name: user.displayName,
+        authProvider: "google",
+        email: user.email,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
 const signInWithEmailAndPassword = async (email, password) => {
   try {
     await auth.signInWithEmailAndPassword(email, password);
@@ -76,6 +113,8 @@ const logout = () => {
 export {
   auth,
   db,
+  facebookProvider,
+  signInWithFacebook,
   signInWithGoogle,
   signInWithEmailAndPassword,
   registerWithEmailAndPassword,
